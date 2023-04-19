dad7b5972e8aa2a1c642c54666db2402a5ca7c98
ades-ship <adeshsengar156@gmail.com>
2023-04-20 00:09:51 +0530

    In React, the useEffect hook takes an optional array of dependencies as its second argument. This array is used to specify the variables that the useEffect callback function depends on.

--- myapp/src/Home.js
+++ myapp/src/Home.js
@@ -1,24 +1,30 @@
-import { useState , useEffect} from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'adesh', id: 1 },
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'adesh', id: 3 }
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
-  const handleDelete=(id)=>{
-    const newblogs=blogs.filter(blog=>blog.id!==id);
-    setBlogs(newblogs);
+  const [name, setName] = useState('adesh');
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
   }
-  useEffect(()=>{
+
+  useEffect(() => {
     console.log('use effect ran');
     console.log(blogs);
-  })
+  }, [name])
+
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('gaurav')}>change name</button>
+    <p>{name}</p>
     </div>
   );
 }
