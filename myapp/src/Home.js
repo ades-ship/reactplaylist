06d103cbcad74210d83780c0bd444ec0f76ff65c
ades-ship <adeshsengar156@gmail.com>
2023-04-19 16:00:30 +0530

    add use state hook

--- myapp/src/Home.js
+++ myapp/src/Home.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [name,setName]=useState('adesh');
+    const [age,setAge]=useState(30);
+    const handleClick=()=>{
+        console.log('hello adesh');
+        setName('gaurav');
+        setAge(25);
+    }
+
+    const handleClickAgain=(name)=>{
+        console.log('hello'+name);
+    }
+
     return ( 
         <div className="Homepage">
             <h2>homepage</h2>
+            <button onClick={handleClick}>click me</button>
+            <p>{name} is {age} years old</p>
+            <button onClick={
+                ()=> handleClickAgain('adesh')
+            }>click me again</button>
         </div>
      );
 }
