01110a77796940d136f7277d8f56c10c3e805e2e
ades-ship <adeshsengar156@gmail.com>
2023-04-19 23:41:42 +0530

    useEffect hook is used to handle side effects in functional components

--- myapp/src/Home.js
+++ myapp/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -12,6 +12,10 @@ const Home = () => {
     const newblogs=blogs.filter(blog=>blog.id!==id);
     setBlogs(newblogs);
   }
+  useEffect(()=>{
+    console.log('use effect ran');
+    console.log(blogs);
+  })
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
